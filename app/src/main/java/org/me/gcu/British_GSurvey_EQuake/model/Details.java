b9cf3ef213d75273855e72ecf0d72764d5795d3a
kirubello <58917531+kirubello@users.noreply.github.com>
2021-04-19 00:41:40 +0100

    FINAL COMMIT

---
+++ app/src/main/java/org/me/gcu/British_GSurvey_EQuake/model/Details.java
@@ -0,0 +1,43 @@
+package org.me.gcu.British_GSurvey_EQuake.model;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.squareup.picasso.Picasso;
+
+import org.me.gcu.British_GSurvey_EQuake.R;
+
+public class Details extends AppCompatActivity {
+    TextView textTitle;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_details);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        Intent i = getIntent();
+        String title = i.getStringExtra("location");
+        String link = i.getStringExtra("link");
+        String pubdate= i.getStringExtra("pubdate");
+
+
+        ImageView imageView=findViewById(R.id.imageView2);
+        String a =link.replace(".html", ".jpg.0");
+        Picasso.get().load(a)
+              .placeholder(R.drawable.bg_grey)
+                .into(imageView);
+
+
+        textTitle = findViewById(R.id.detailTitle);
+        textTitle.setText(title);
+        textTitle = findViewById(R.id.pubdate);
+        textTitle.setText(pubdate);
+
+
+    }
+}
